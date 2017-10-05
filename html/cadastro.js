2514b16c5011dc0035fcfa9e1b2fe496bfb362b4
FelipeIvanaga <30486121+FelipeIvanaga@users.noreply.github.com>
2017-10-05 12:36:19 -0300

    adicionando required no campos necessarios

--- html/cadastro.js
+++ html/cadastro.js
@@ -1,15 +1,1 @@
-function check_info() {
-  var nome = document.getElementById('nome').value;
-  var sobrenome = document.getElementById('sobrenome').value;
-  var cpf = document.getElementById('cpf').value;
-  var telefone = document.getElementById('telefone').value;
-  
-  if (nome == "" || sobrenome == "" || cpf == "" || telefone == ""){
-    alert('Complete os campos obrigatorios(*)')
-    return false;
-  }
-  else{
-    return true;
-  }
-  
-}
+
